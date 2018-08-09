4106803e470a9dfe8d809187820114790e332a64
yungshun317 <seraph317@gmail.com>
2018-08-09 21:32:58 +0800

    Create a local top N list for each input partition

--- src/main/scala/yungshun/chang/topnnonunique/TopNNonunique.scala
+++ src/main/scala/yungshun/chang/topnnonunique/TopNNonunique.scala
@@ -13,5 +13,31 @@ object TopNNonunique {
 
     val sparkConf = new SparkConf().setAppName("TopNNonunique")
     val sc = new SparkContext(sparkConf)
+
+    val N = sc.broadcast(2)
+    val path = args(0)
+
+    val input = sc.textFile(path)
+    val kv = input.map(line => {
+      val tokens = line.split(",")
+      (tokens(0), tokens(1).toInt)
+    })
+
+    val uniqueKeys = kv.reduceByKey(_ + _)
+    import Ordering.Implicits._
+    val partitions = uniqueKeys.mapPartitions(itr => {
+      var sortedMap = SortedMap.empty[Int, String]
+      itr.foreach { tuple =>
+      {
+        sortedMap += tuple.swap
+        if (sortedMap.size > N.value) {
+          sortedMap = sortedMap.takeRight(N.value)
+        }
+      }
+      }
+      sortedMap.takeRight(N.value).toIterator
+    })
+
+    val alltop10 = partitions.collect()
   }
 }
